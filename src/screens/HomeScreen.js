cfe40e7392dd5b1daae53ab98a6492df42b6bd61
Tony <tony.tong.210@my.csun.edu>
2020-11-16 20:46:10 -0800

    Finished some of newSession

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import s from '../styles/global'
 import React, { useState, useContext } from 'react';
-import { 
+import {
     View,
     Text,
     StyleSheet,
@@ -21,35 +21,35 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 const { height, width } = Dimensions.get('window');
 
 
-export default function HomeScreen(props){
+export default function HomeScreen(props) {
     const { user } = useContext(AuthContext);
-    
+
 
     return (
-        <View style={s.global}>  
+        <View style={s.global}>
             <CollapsibleHeaderScrollView
                 CollapsibleHeaderComponent={
                     <Header
-                        statusBarProps={{ 
-                            backgroundColor: '#202020', 
-                            translucent: true, 
+                        statusBarProps={{
+                            backgroundColor: '#202020',
+                            translucent: true,
                             barStyle: 'light-content'
                         }}
 
-                        leftComponent={{ 
+                        leftComponent={{
                             icon: 'menu',
-                            color: '#C2185B', 
+                            color: '#C2185B',
                             underlayColor: '#282828',
                             onPress: props.navigation.openDrawer
                         }}
 
                         centerComponent={
                             {
-                                text: 'PokerTracker', 
-                                style: { 
-                                    color: '#C2185B', 
-                                    fontSize: 25 
-                                } 
+                                text: 'PokerTracker',
+                                style: {
+                                    color: '#C2185B',
+                                    fontSize: 25
+                                }
                             }
                         }
 
@@ -58,15 +58,15 @@ export default function HomeScreen(props){
                                 name='user'
                                 type='font-awesome'
                                 color='#C2185B'
-                                underlayColor= '#282828'
+                                underlayColor='#282828'
                                 onPress={() => console.log("do nothing")} />
                         }
-        
+
                         containerStyle={{
                             height: 80,
                             backgroundColor: '#282828',
-                            borderBottomColor: '#282828', 
-                            borderBottomWidth: 1 
+                            borderBottomColor: '#282828',
+                            borderBottomWidth: 1
                         }}
 
                     />
@@ -74,17 +74,17 @@ export default function HomeScreen(props){
                 headerHeight={80}
                 disableHeaderSnap={true}
             >
-            
-            
+
+
                 <SafeAreaView>
                     <ScrollView scrollEventThrottle={16}>
                         <View>
-                            {user && 
+                            {user &&
                                 <View>
-                                   <Text style={styles.textAlt}>
+                                    <Text style={styles.textAlt}>
                                         Hello {user.name}!
-                                    </Text> 
-                            
+                                    </Text>
+
 
                                     <TouchableOpacity
                                         style={styles.button}
@@ -97,19 +97,19 @@ export default function HomeScreen(props){
 
                                     <TouchableOpacity
                                         style={styles.button}
-                                        onPress={()=>{
-                                           props.navigation.navigate('SessionDetails', 
-                                                {   
+                                        onPress={() => {
+                                            props.navigation.navigate('SessionDetails',
+                                                {
                                                     gameType: 'No Limit Hold\'em',
                                                     startTime: '10/15/20  11:00 AM',
                                                     endTime: '10/15/20  1:00 PM',
                                                     buyIn: '- $100',
                                                     cashOut: '+ $200'
-                                                    });
-                                            }
-                                       }
-                                    >   
-                                        
+                                                });
+                                        }
+                                        }
+                                    >
+
                                         <View style={styles.row}>
                                             <Text style={styles.text} >Aria</Text>
                                             <Text style={styles.textLeft} >10/5/20</Text>
@@ -119,23 +119,23 @@ export default function HomeScreen(props){
                                             <Text style={styles.text} >No Limit Hold'em</Text>
                                             <Text style={styles.textLeft} >+ $200</Text>
                                         </View>
-                                         
+
                                     </TouchableOpacity>
 
                                     <TouchableOpacity
                                         style={styles.button}
-                                        onPress={()=>{
-                                            props.navigation.navigate('SessionDetails', 
-                                                 {   
-                                                     gameType: 'PLO Omaha',
-                                                     startTime: '9/27/20  11:35 PM',
-                                                     endTime: '9/28/20  1:00 AM',
-                                                     buyIn: '- $100',
-                                                     cashOut: '- $150'
-                                                     });
-                                             }
+                                        onPress={() => {
+                                            props.navigation.navigate('SessionDetails',
+                                                {
+                                                    gameType: 'PLO Omaha',
+                                                    startTime: '9/27/20  11:35 PM',
+                                                    endTime: '9/28/20  1:00 AM',
+                                                    buyIn: '- $100',
+                                                    cashOut: '- $150'
+                                                });
                                         }
-                                    >   
+                                        }
+                                    >
                                         <View style={styles.row}>
                                             <Text style={styles.text} >Bellagio</Text>
                                             <Text style={styles.textLeft} >9/27/20</Text>
@@ -145,24 +145,24 @@ export default function HomeScreen(props){
                                             <Text style={styles.text} >PLO Omaha</Text>
                                             <Text style={styles.textLeft} >- $150</Text>
                                         </View>
-                                         
+
                                     </TouchableOpacity>
 
                                     <TouchableOpacity
                                         style={styles.button}
-                                        onPress={()=>{
-                                            props.navigation.navigate('SessionDetails', 
-                                                 {   
-                                                     gameType: 'Limit Hold\'em',
-                                                     startTime: '9/10/20  5:00 PM',
-                                                     endTime: '9/10/20  8:00 PM',
-                                                     buyIn: '- $50',
-                                                     cashOut: '- $150'
-                                                     });
-                                             }
+                                        onPress={() => {
+                                            props.navigation.navigate('SessionDetails',
+                                                {
+                                                    gameType: 'Limit Hold\'em',
+                                                    startTime: '9/10/20  5:00 PM',
+                                                    endTime: '9/10/20  8:00 PM',
+                                                    buyIn: '- $50',
+                                                    cashOut: '- $150'
+                                                });
                                         }
-                                        
-                                    >   
+                                        }
+
+                                    >
                                         <View style={styles.row}>
                                             <Text style={styles.text} >Caesar's Palace</Text>
                                             <Text style={styles.textLeft} >9/10/20</Text>
@@ -172,15 +172,15 @@ export default function HomeScreen(props){
                                             <Text style={styles.text} >Limit Hold'em</Text>
                                             <Text style={styles.textLeft} >+ $67</Text>
                                         </View>
-                                 
+
                                     </TouchableOpacity>
-                                    
+
                                 </View>
                             }
                         </View>
                     </ScrollView>
                 </SafeAreaView>
-            
+
             </CollapsibleHeaderScrollView>
         </View>
     );
@@ -190,8 +190,8 @@ export default function HomeScreen(props){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:'center', 
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         height: 500
     },
     row: {
@@ -203,7 +203,7 @@ const styles = StyleSheet.create({
         flex: 1,
         color: 'black',
         fontSize: 20,
-        margin: 20, 
+        margin: 20,
     },
     textLeft: {
         color: 'black',
@@ -211,10 +211,10 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     textAlt: {
-        fontSize: 24, 
-        fontWeight: '700', 
-        color: 'black', 
-        flex:1, 
+        fontSize: 24,
+        fontWeight: '700',
+        color: 'black',
+        flex: 1,
         marginLeft: 10,
         margin: 20
     },
@@ -227,45 +227,45 @@ const styles = StyleSheet.create({
         color: "red",
         margin: 20
     },
-    title: { 
-        fontSize: 24, 
-        fontWeight: '700', 
+    title: {
+        fontSize: 24,
+        fontWeight: '700',
         color: 'black'
     },
     placeContainer: {
-        width:width-40,
-        height:width/2, 
-        borderWidth:0.5, 
+        width: width - 40,
+        height: width / 2,
+        borderWidth: 0.5,
         borderColor: '#dddddd',
         borderRadius: 4,
         marginVertical: 10,
-        overflow:"hidden"
+        overflow: "hidden"
     }
 });
 
 const loginStyles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
     },
     header: {
-      fontSize: 25
+        fontSize: 25
     },
     image: {
-      marginTop: 15,
-      width: 150,
-      height: 150,
-      borderColor: "rgba(0,0,0,0.2)",
-      borderWidth: 3,
-      borderRadius: 150
+        marginTop: 15,
+        width: 150,
+        height: 150,
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 3,
+        borderRadius: 150
     },
     flatview: {
-      justifyContent: 'center',
-      paddingTop: 30,
-      borderRadius: 2,
+        justifyContent: 'center',
+        paddingTop: 30,
+        borderRadius: 2,
     },
     text: {
         color: 'white',
--- src/screens/NewSessionScreen.js
+++ src/screens/NewSessionScreen.js
@@ -1,70 +1,110 @@
 import s from '../styles/global'
-import React, { useState, useContext } from 'react';
-import { 
+import React, { useState, useContext, Component, } from 'react';
+//import React, { Component } from 'react';
+
+import {
     View,
     Text,
     StyleSheet,
     Platform,
     SafeAreaView,
     ScrollView,
-    Dimensions
+    Dimensions,
+    TouchableOpacity,
+    Button,
+    SectionList,
+    TextInput
 } from 'react-native';
 import { CollapsibleHeaderScrollView } from 'react-native-collapsible-header-views';
 import { Header } from 'react-native-elements';
 import Constants from 'expo-constants';
 import { AuthContext } from '../../context';
+import { TouchableOpacityComponent } from 'react-native';
+//import { Dropdown } from 'react-native-material-dropdown';
 
 
 const { height, width } = Dimensions.get('window');
 
+/*class Data extends Component {
+    render() {
+        let data = [{
+            value: 'Teaxs Hold-em',
+        }, {
+            value: 'Poker',
+        }, {
+            value: 'PLO Omaha',
+        }];
 
-export default function NewSessionScreen(props){
+
+    }
+}*/
+const DATA = [
+    {
+        data: ['Hold-em', 'Poker', 'PLO Omaha']
+    }
+];
+
+
+
+
+export default function NewSessionScreen(props) {
     const { user } = useContext(AuthContext);
-    
+
 
     return (
-        <View style={s.global}>  
+        <View style={s.global}>
             <CollapsibleHeaderScrollView
                 CollapsibleHeaderComponent={
                     <Header
-                        statusBarProps={{ 
-                            backgroundColor:'#202020', 
-                            translucent:true, 
-                            barStyle:'light-content'
+                        statusBarProps={{
+                            backgroundColor: '#202020',
+                            translucent: true,
+                            barStyle: 'light-content'
                         }}
-                        leftComponent={{ 
+                        leftComponent={{
                             icon: 'chevron-left',
-                            color: '#C2185B', 
+                            color: '#C2185B',
                             underlayColor: '#282828',
                             onPress: () => props.navigation.goBack()
                         }}
-                        // centerComponent={{ 
-                        //     text: 'BarHop', 
-                        //     style: { 
-                        //         color: '#C2185B', 
-                        //         fontSize: 25 
-                        //     } 
-                        // }}
                         containerStyle={{
                             height: 80,
                             backgroundColor: '#282828',
-                            borderBottomColor:'#282828', 
-                            borderBottomWidth:1 
+                            borderBottomColor: '#282828',
+                            borderBottomWidth: 1
                         }}
-                    
+
                     />
                 }
                 headerHeight={80}
                 disableHeaderSnap={true}
-            >            
+            >
                 <SafeAreaView>
                     <ScrollView scrollEventThrottle={16}>
                         <View>
-                            {user && 
-                                <Text style={styles.textAlt}>
-                                    Welcome to Session Screen
-                                </Text>
-                            }                              
+                            {user &&
+                                <View style={styles.container1}>
+                                    <View style={styles.row}>
+                                        <Text style={styles.label} >Game Type</Text>
+                                        <Text style={styles.text}>{ }</Text>
+                                    </View>
+
+                                    <View style={styles.row}>
+                                        <TextInput style={styles.labelAlt} placeholder="Buy In" keyboardType="decimal-pad" ></TextInput>
+                                        <TextInput style={styles.labelAlt} placeholder="Cash Out" ></TextInput>
+                                    </View>
+
+                                    <View style={styles.row}>
+                                        <Text style={styles.label} >Start Time</Text>
+                                        <TextInput style={styles.textTime} ></TextInput>
+                                    </View>
+
+                                    <View style={styles.row}>
+                                        <Text style={styles.label} >End Time</Text>
+                                        <TextInput style={styles.textTime} >{ }</TextInput>
+                                    </View>
+                                </View>
+                            }
                         </View>
                     </ScrollView>
                 </SafeAreaView>
@@ -77,61 +117,120 @@ export default function NewSessionScreen(props){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:'center', 
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
         height: 500
     },
-    text: {
-        color: 'black',
-        fontSize: 20,
+    container1: {
+        flex: 1,
+        backgroundColor: "#DDDDDD",
         margin: 20
     },
-    textAlt: {
-        fontSize: 24, 
-        fontWeight: '700', 
-        color: 'black', 
-        flex:1, 
-        marginLeft: 10
+    row: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
     },
-    title: { 
-        fontSize: 24, 
-        fontWeight: '700', 
+    text: {
+        flex: 1,
+        color: 'black',
+        fontSize: 20,
+        margin: 20,
+        textAlign: "center",
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        backgroundColor: "darkgray",
+        alignSelf: "flex-start"
+
+    },
+    textAlt: {
+        fontSize: 24,
+        fontWeight: '700',
+        color: 'black',
+        flex: 1,
+        marginLeft: 10,
+        marginTop: 20,
+    },
+    textTime: {
+        flex: 1,
+        color: 'black',
+        fontSize: 20,
+        margin: 20,
+        paddingHorizontal: 30,
+        paddingTop: 5,
+        paddingBottom: 5,
+        textAlign: "center",
+        backgroundColor: "darkgray",
+        alignSelf: "flex-start"
+    },
+    textAmount: {
+        flex: 1,
+        color: 'black',
+        fontSize: 20,
+        margin: 25,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 0,
+        marginHorizontal: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 25,
+        textAlign: "center",
+        backgroundColor: "darkgray"
+    },
+    label: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: '600',
+        margin: 20,
+        paddingVertical: 20
+
+    },
+    labelAlt: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: '600',
+        margin: 20,
+        paddingVertical: 25
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '700',
         color: 'black'
     },
     placeContainer: {
-        width:width-40,
-        height:width/2, 
-        borderWidth:0.5, 
+        width: width - 40,
+        height: width / 2,
+        borderWidth: 0.5,
         borderColor: '#dddddd',
         borderRadius: 4,
         marginVertical: 10,
-        overflow:"hidden"
+        overflow: "hidden"
     }
 });
 
 const loginStyles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-      paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
     },
     header: {
-      fontSize: 25
+        fontSize: 25
     },
     image: {
-      marginTop: 15,
-      width: 150,
-      height: 150,
-      borderColor: "rgba(0,0,0,0.2)",
-      borderWidth: 3,
-      borderRadius: 150
+        marginTop: 15,
+        width: 150,
+        height: 150,
+        borderColor: "rgba(0,0,0,0.2)",
+        borderWidth: 3,
+        borderRadius: 150
     },
     flatview: {
-      justifyContent: 'center',
-      paddingTop: 30,
-      borderRadius: 2,
+        justifyContent: 'center',
+        paddingTop: 30,
+        borderRadius: 2,
     },
     text: {
         color: 'white',
